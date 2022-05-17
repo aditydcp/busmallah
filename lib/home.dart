ad24fdc2d70afa27d93559293605ca9a7beb9519
bimoajif <bimoajifajrianto@gmail.com>
2022-05-17 15:10:01 +0700

    update home.dart

--- lib/home.dart
+++ lib/home.dart
@@ -59,11 +59,198 @@ class _HomeScreenState extends State<HomeScreen> with SingleTickerProviderStateM
       body: TabBarView(
         controller: _tabController,
         children: const [
-          DalamKota(),
-          Center(
-            child: Text('Hello World'),
-          )
+          AntarKota(),
+          DalamKota()
         ]
+      ),
+    );
+  }
+}
+
+class AntarKota extends StatefulWidget {
+  const AntarKota({ Key? key }) : super(key: key);
+
+  @override
+  State<AntarKota> createState() => _AntarKotaState();
+}
+
+class _AntarKotaState extends State<AntarKota> {
+
+  var jumlahPenumpang = 1;
+  final TextEditingController _controllerPenumpang = TextEditingController();
+
+  void addPenumpang(){
+    setState(() {
+      jumlahPenumpang++;
+    });
+  }
+
+  void removePenumpang(){
+    setState(() {
+      jumlahPenumpang <= 1 ? jumlahPenumpang = 1 : jumlahPenumpang--;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // for debugging
+    // log(jumlahPenumpang.toString());
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Card(
+              elevation: 0,
+              child: Container(
+                height: MediaQuery.of(context).size.height/7,
+                decoration: BoxDecoration(
+                  image: const DecorationImage(
+                    image: AssetImage('assets/images/Header.png'),
+                    fit: BoxFit.fitWidth,
+                  ),
+                  borderRadius: BorderRadius.circular(10)
+                )
+              )
+            ),
+            Card(
+              elevation: 30,
+              shadowColor: const Color(0xFFF0F0F0),
+              color: Colors.white,
+              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+              child: Container(
+                margin: EdgeInsets.symmetric(vertical: MediaQuery.of(context).size.height/30, horizontal: 18),
+                child: Center(
+                  child: Column(
+                    children: [
+                      const Text(
+                        'ANTAR KOTA',
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.w800
+                        ),
+                      ),
+                      Divider(height: (MediaQuery.of(context).size.height/60), color: Colors.transparent,),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Flexible(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                const Text('Asal'),
+                                const Divider(height: 5, color: Colors.transparent),
+                                TextField(
+                                  onChanged: (value) {},
+                                  decoration: const InputDecoration(
+                                    border: OutlineInputBorder(),
+                                    contentPadding: EdgeInsets.symmetric(vertical: 8, horizontal: 15)
+                                  ),
+                                )
+                              ],
+                            ),
+                          ),
+                          const SizedBox(width: 15,),
+                          Flexible(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                const Text('Tujuan'),
+                                const Divider(height: 5, color: Colors.transparent),
+                                TextField(
+                                  onChanged: (value) {},
+                                  decoration: const InputDecoration(
+                                    border: OutlineInputBorder(),
+                                    contentPadding: EdgeInsets.symmetric(vertical: 8, horizontal: 15)
+                                  ),
+                                )
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                      Divider(height: MediaQuery.of(context).size.height/60, color: Colors.transparent,),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const Text('Tanggal Berangkat'),
+                          const Divider(height: 5, color: Colors.transparent,),
+                          TextField(
+                            onChanged: (value){},
+                            decoration: const InputDecoration(
+                              border: OutlineInputBorder(),
+                              contentPadding: EdgeInsets.symmetric(vertical: 8, horizontal: 15)
+                            ),
+                          )
+                        ],
+                      ),
+                      Divider(height: MediaQuery.of(context).size.height/60, color: Colors.transparent,),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const Text('Jumlah Penumpang'),
+                          const Divider(height: 5, color: Colors.transparent,),
+                          TextField(
+                            controller: _controllerPenumpang,
+                            onChanged: (value) {},
+                            readOnly: true,
+                            decoration: InputDecoration(
+                              hintText: jumlahPenumpang.toString()+' Penumpang',
+                              hintStyle: const TextStyle(color: Colors.black),
+                              
+                              suffixIcon: Row(
+                                mainAxisAlignment: MainAxisAlignment.end,
+                                mainAxisSize: MainAxisSize.min,
+                                children: [
+                                  IconButton(
+                                    onPressed: removePenumpang,
+                                    icon: const Icon(
+                                      Icons.remove,
+                                      color: Colors.black
+                                    ),
+                                  ),
+                                  IconButton(
+                                    onPressed: addPenumpang,
+                                    icon: const Icon(
+                                      Icons.add,
+                                      color: Colors.black,
+                                    )
+                                  )
+                                ],
+                              ),
+                              
+                              border: const OutlineInputBorder(),
+                              contentPadding: const EdgeInsets.symmetric(vertical: 8, horizontal: 15)
+                            ),
+                          )
+                        ],
+                      ),
+                      Divider(height: MediaQuery.of(context).size.height/30, color: Colors.transparent,),
+                      ElevatedButton(
+                        onPressed: (){}, 
+                        child: const Text(
+                          'Cari Tiket',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold
+                          )
+                        ),
+                        style: ButtonStyle(
+                          backgroundColor: MaterialStateProperty.all<Color>(const Color(0xFFE57259)),
+                          foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
+                          shadowColor: MaterialStateProperty.all<Color>(const Color(0x55E57259)),
+                          elevation: MaterialStateProperty.all<double>(15),
+                          shape: MaterialStateProperty.all<OutlinedBorder>(RoundedRectangleBorder(borderRadius: BorderRadius.circular(10))),
+                          fixedSize: MaterialStateProperty.all<Size>(Size(MediaQuery.of(context).size.width,50))
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
@@ -127,7 +314,7 @@ class _DalamKotaState extends State<DalamKota> {
                   child: Column(
                     children: [
                       const Text(
-                        'ANTAR KOTA',
+                        'DALAM KOTA',
                         style: TextStyle(
                           fontSize: 18,
                           fontWeight: FontWeight.w800
